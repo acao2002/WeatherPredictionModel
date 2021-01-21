4ca82d20f9300b53b542f410f8352da3558976bd
acao2002 <xuanan12072002@gmail.com>
2021-01-21 06:02:18 -0500

    added predict function

--- WeatherPrediction/WeatherPrediction.py
+++ WeatherPrediction/WeatherPrediction.py
@@ -1,6 +1,14 @@
 import numpy as np 
 
 Pmatrix = np.array([[0.7,0.5,0.2],[0.2,0.3,0.5],[0.1,0.2,0.3]],np.float32)
-today = np.array([[0],[1],[0]],np.float32)
+today = np.array([[0],[0],[0]],np.float32)
 prediction = np.matmul(Pmatrix,today)
-print(prediction)
+
+def predict(state,days):
+    today[state] = 1
+    for i in range(days-1):
+        global Pmatrix
+        Pmatrix = np.matmul(Pmatrix,Pmatrix)
+    prediction = np.matmul(Pmatrix,today)
+    return prediction
+print(predict(0,6))
