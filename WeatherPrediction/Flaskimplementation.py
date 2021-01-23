cd3b0badc27916c1bccd3aa116250f2db270e77c
acao2002 <xuanan12072002@gmail.com>
2021-01-23 06:58:33 -0500

    working on deployment

--- WeatherPrediction/Flaskimplementation.py
+++ WeatherPrediction/Flaskimplementation.py
@@ -1,18 +1,29 @@
-from flask import Flask , render_template, request
+from flask import Flask , render_template, request, redirect, url_for
 app = Flask(__name__)
+from WeatherPrediction import weather
 
 @app.route('/', methods = ["GET", "POST"])
 
 def index():
-    last_name = ''
-    first_name =''
+    state = 0
+    days =0
+    sunny = 0 
+    rainy = 0 
+    cloudy = 0
     if request.method == "POST": 
-       # getting input with name = fname in HTML form 
-       first_name = request.form.get("fname") 
-       # getting input with name = lname in HTML form  
-       last_name = request.form.get("lname")  
-
-    return render_template('index.html',variable =first_name,variable2 = last_name)
+        if 'submitname' in request.form:
+        # getting input with name = fname in HTML form 
+                state = int(request.form.get("fname") )
+        # getting input with name = lname in HTML form  
+                days = int(request.form.get("lname") )
+                Weather = weather() 
+                prediction = Weather.predict(state,days) 
+                sunny = prediction[0]*100
+                rainy = prediction[1]*100
+                cloudy = prediction[2]*100
+        else: 
+            pass
+    return render_template('index.html',var = sunny ,var1 = rainy,var2 = cloudy)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- WeatherPrediction/WeatherPrediction.py
+++ WeatherPrediction/WeatherPrediction.py
@@ -2,15 +2,12 @@ import numpy as np
 
 Pmatrix = np.array([[0.7,0.5,0.2],[0.2,0.3,0.5],[0.1,0.2,0.3]],np.float32)
 today = np.array([[0],[0],[0]],np.float32)
-prediction = np.matmul(Pmatrix,today)
 
-def predict(state,days):
-    today[state] = 1
-    for i in range(days-1):
-        global Pmatrix
-        Pmatrix = np.matmul(Pmatrix,Pmatrix)
-    prediction = np.matmul(Pmatrix,today)
-    return prediction
-
-    
-print(predict(0,6))
+class weather:
+    def predict(self,state,days):
+        today[state] = 1
+        for i in range(days-1):
+            global Pmatrix
+            Pmatrix = np.matmul(Pmatrix,Pmatrix)
+        prediction = np.matmul(Pmatrix,today)
+        return prediction
